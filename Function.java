3a389f458a97cc25702053180ae453eba5dd6feb
forthelion <igorj1@live.com>
2024-06-08 15:45:10 -0700

    making function getassignment and gradeweighting

--- Function.java
+++ Function.java
@@ -3,15 +3,26 @@ public class Function {
 
 
     // ASK SYLLABUS
+    getSyllabus(){
+
+    }
     
     
     //IMPORT ALL ASSIGNMENT / DESCRIPTIONS
+    getAssignmentList(){
 
+    }
 
     //WEIGHT OF EACH GRADE 
+    /// i presume that i getting a list of grades , list of types and how much each section balance
+    // structure  grades [homework["here are grades"1,2,3,4,5,6],quizz]
+    gradeWeighting(){
 
+    }
 
     //IMPORT READING
+    
+
 
 
 
